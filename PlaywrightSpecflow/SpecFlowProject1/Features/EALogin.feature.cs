4a715c1a2e430ef92e45d3ee27b21b79216b33e6
Todd Dyjak <tdyjak@gmail.com>
2025-06-05 15:03:18 -0600

    update proj name

--- PlaywrightSpecflow/SpecFlowProject1/Features/EALogin.feature.cs
+++ PlaywrightSpecflow/SpecFlowProject1/Features/EALogin.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace SpecFlowProject1.Features
+namespace SpecFlowProject.Features
 {
     using TechTalk.SpecFlow;
     using System;
